46e3f33ae92b7fbe18a26cb7387eddc412f55cbc
RNowak4 <RNowak4@users.noreply.github.com>
2015-03-12 01:16:43 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
-#include <iostream>
+#include "Model.h"
 
-using namespace std; 
+/* Do implementacji w Controllerze:
+ *-Klasa C powinna dostarczyc interfejs do "zabicia"
+ * Jednostki. UpdateStatus moze np. zabic jednostke this,
+ * albo zabic inna jednostke. Wtedy wysyla do Modelu wiadomosc
+ * o tym, a Model usuwa podane jednostki. Nastepnie wywoluje metode
+ * Controllera, ktora ma go powiadomic o koniecznosci usuniecia jednostki
+ * z ekranu.
+ * 
+ *-W C powinna byc metoda, ktora wysyla pojemnik z wskaznikami
+ * na jednostki, ktore znajduja sie w obrebie zadanego kwadratu lub okregu.
+ */
+
+/* Do implementacji w Modelu:
+ * 
+ */
 
 int main(int argc, char** argv) {
+    
     return 0;
 }
-
