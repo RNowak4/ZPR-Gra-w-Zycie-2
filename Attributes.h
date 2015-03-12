cce0b9bd704e0c432d0a7175def52e723057ff9f
AngryProbe <Radek095@gmail.com>
2015-03-12 01:06:38 +0100

    Bardzo pierwszy zarys tej klasy. Oczywiscie komentarze zmienie na angielskie i w skladni doxygen'a.

---
+++ Attributes.h
@@ -0,0 +1,37 @@
+/* 
+ * File:   Attributes.h
+ * Author: Radek
+ *
+ * Created on 12 marca 2015, 00:37
+ */
+
+#ifndef ATTRIBUTES_H
+#define	ATTRIBUTES_H
+
+#include <chrono>
+#include <random>
+#include <vector>
+
+class Attributes {
+private:
+    double strength_;
+    double speed_;
+    double fertility_; // Plodnosc
+    double hunger_; // Glod?
+    double sightLen_; // Jak daleko jednostka widzi = wys trojkata
+    unsigned lifeLen_; // Dlugosc zycia wyrazona w ustalonych kwantach czasu
+    // Tak jak w ukladzie wspolrzednych kartezjanskich 2D.
+    unsigned sightRad_; 
+    // Funkcja, ktora generuje atrybuty zupelnie losowo, bez przodkow
+    // Wywolana, gdy vector atrybutow jest pusty
+    void generateAttributes();
+    
+public:
+    Attributes();
+    Attributes(const Attributes& orig);
+    virtual ~Attributes();
+    // Funkcja, ktora generuje atrybuty na podstawie przodkow
+    void inheritAttributes(std::vector<Attributes>);
+};
+
+#endif	/* ATTRIBUTES_H */
