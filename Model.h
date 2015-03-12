1d6f594d2e57655ea5efc96312921c30c2f84367
AngryProbe <Radek095@gmail.com>
2015-03-12 01:05:22 +0100

    Bardzo pierwszy zarys tej klasy. Oczywiscie komentarze zmienie na angielskie i w skladni doxygen'a.

---
+++ Model.h
@@ -0,0 +1,33 @@
+/* 
+ * File:   Model.h
+ * Author: Radek
+ *
+ * Created on 9 marca 2015, 21:29
+ */
+
+#ifndef MODEL_H
+#define	MODEL_H
+
+#include "Animal.h"
+#include <list>
+
+/* Model bedzie wysylal cale paczki danych
+ * do Controllera. Pewnie w postaci listy.
+ */
+
+class Model {
+public:
+    Model();
+    Model(const Model& orig);
+    ~Model();
+    // Funkcja, ktora tworzy zwierze o konkretnych parametrach.
+    Animal* createAnimal();
+    // Update'uje statusy wszystkich jednostek
+    void updateAnimalsStatuses();
+    
+private:
+    std::list<Animal*> animalList_;
+};
+
+#endif	/* MODEL_H */
+
