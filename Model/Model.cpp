fddb4a9b5721a588f41de22f6f86fbfd4aa14e7d
rnowak4 <radek095@gmail.com>
2015-05-17 15:05:33 +0200

    a

--- Model/Model.cpp
+++ Model/Model.cpp
@@ -80,7 +80,7 @@ shared_ptr<AnimalData> Model::getAnimalData(int x, int y) {
 			animalPtr = animal;
 			break;
 		}*/
-		animalPtr = animal; //chujki
+		animalPtr = animal; //chujkisad
 	}
 
 	// Nie znaleziono
