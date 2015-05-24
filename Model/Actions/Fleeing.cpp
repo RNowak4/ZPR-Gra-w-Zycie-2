4dcb5570aa2c1d438866483994c46b2b1978fd12
Radek <Radek095@gmail.com>
2015-05-24 03:14:41 +0200

    Nowa akcja - uciekanie. jeszcze nietestowana. Z pelna funcjonalnoscia.

---
+++ Model/Actions/Fleeing.cpp
@@ -0,0 +1,52 @@
+/*
+ * Fleeing.cpp
+ *
+ *  Created on: 24 maj 2015
+ *      Author: radek
+ */
+
+#include "Fleeing.h"
+
+#include "../Animal.h"
+#include "../Model.h"
+#include "../ViewStructs.h"
+#include "TestAction.h"
+
+Fleeing::Fleeing(Animal* animalPtr_, Animal* hunter_) :
+		Action(animalPtr_), hunter(hunter_) {
+	animalPtr->setVelocity(1.5);
+	this->animalPtr->setLookingAngle(
+			Model::countAngle(this->animalPtr->returnCoodtinates(),
+					hunter->returnCoodtinates()) + 180);
+	this->animalPtr->stopTurning();
+}
+
+Fleeing::~Fleeing() {
+	modelPtr->deleteAction(this);
+}
+
+void Fleeing::performAction() {
+	if (hunter != nullptr) {
+		this->animalPtr->setLookingAngle(
+				Model::countAngle(this->animalPtr->returnCoodtinates(),
+						hunter->returnCoodtinates()) + 180);
+
+		if (Model::countDistance(this->animalPtr->returnCoodtinates(),
+				hunter->returnCoodtinates())
+				> this->animalPtr->returnLocationData()->sightLen_) {
+			hunter = nullptr;
+		}
+	}
+}
+
+Action* Fleeing::chooseNextAction() {
+	if (hunter == nullptr)
+		return new TestAction(animalPtr);
+
+	return this;
+}
+
+void Fleeing::deleteAnimal(const Animal* toDelete) {
+	if (hunter == toDelete)
+		hunter = nullptr;
+}
---
+++ Model/Actions/Fleeing.h
@@ -0,0 +1,25 @@
+/*
+ * Fleeing.h
+ *
+ *  Created on: 24 maj 2015
+ *      Author: radek
+ */
+
+#ifndef MODEL_ACTIONS_FLEEING_H_
+#define MODEL_ACTIONS_FLEEING_H_
+
+#include "Action.h"
+
+class Fleeing: public Action {
+private:
+	Animal* hunter;
+
+public:
+	Fleeing(Animal* animalPtr_, Animal* hunter_);
+	virtual ~Fleeing();
+	void performAction();
+	Action* chooseNextAction();
+	void deleteAnimal(const Animal*);
+};
+
+#endif /* MODEL_ACTIONS_FLEEING_H_ */
