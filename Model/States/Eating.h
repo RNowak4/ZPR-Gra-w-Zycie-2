91cea4d8f4410c34696698b913ec2f557d67f18a
Radek <Radek095@gmail.com>
2015-05-24 02:48:20 +0200

    Nowy Stan

---
+++ Model/States/Eating.h
@@ -0,0 +1,26 @@
+/*
+ * Eating.h
+ *
+ *  Created on: 24 maj 2015
+ *      Author: radek
+ */
+
+#ifndef MODEL_STATES_EATING_H_
+#define MODEL_STATES_EATING_H_
+
+#include <string>
+
+#include "State.h"
+
+class Eating: public State {
+private:
+	static const unsigned decreaseValue = 200;
+
+public:
+	Eating(Animal* animalPtr);
+	~Eating();
+	bool isThatMe(string stateName);
+	string toString();
+};
+
+#endif /* MODEL_STATES_EATING_H_ */
