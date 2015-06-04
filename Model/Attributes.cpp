41293d9767be97bb854d26717b703f31215075c0
Radek <Radek095@gmail.com>
2015-06-04 18:35:16 +0200

    zmiany zmiany

--- Model/Attributes.cpp
+++ Model/Attributes.cpp
@@ -7,15 +7,23 @@
 
 #include "Attributes.h"
 
+void normalize(int& value) {
+	if (value < 0)
+		value = 0;
+}
+
+void normalize(double& value) {
+	if (value < 0.0)
+		value = 0.0;
+}
+
 Attributes::Attributes() {
 	strength_ = distribution(generator);
 	maximalSpeed_ = distribution(generator);
 	fertility_ = static_cast<int>(distribution(generator));
 	sickChance_ = sickChanceDistribution(generator);
 
-
 	sightAngle_ = static_cast<int>(distribution(generator) * 360.0 / 20.0);
-
 
 	sightLength_ = static_cast<int>(distribution(generator) * 400.0 / 6.0);
 	hearingRange_ = static_cast<int>(distribution(generator) * 400.0 / 6.0);
@@ -28,6 +36,15 @@ Attributes::Attributes() {
 			/ 30.0;
 
 	lifeLen_ = lifeDistribution(generator);
+
+	normalize(strength_);
+	normalize(maximalSpeed_);
+	normalize(fertility_);
+	normalize(sickChance_);
+	normalize(hearingRange_);
+	normalize(sleepNeed_);
+	normalize(eatNeed_);
+	normalize(lifeLen_);
 }
 
 default_random_engine Attributes::generator;
@@ -39,39 +56,37 @@ normal_distribution<double> Attributes::mutationDistribution(0, 0.2);
 normal_distribution<double> Attributes::mutationLifeDistribution(0, 10);
 
 Attributes Attributes::inheritAttributes(
-		const Attributes& secondAttributes) const {
-	Attributes newAttributes;
+	const Attributes& secondAttributes) const {
+Attributes newAttributes;
 
-	newAttributes.strength_ = (this->strength_ + secondAttributes.strength_)
-			/ 2.0 + mutationDistribution(generator);
+newAttributes.strength_ = (this->strength_ + secondAttributes.strength_) / 2.0
+		+ mutationDistribution(generator);
 
-	newAttributes.fertility_ = (this->fertility_ + secondAttributes.fertility_)
-			/ 2.0 + mutationDistribution(generator) * 4.0;
+newAttributes.fertility_ = (this->fertility_ + secondAttributes.fertility_)
+		/ 2.0 + mutationDistribution(generator) * 4.0;
 
-	newAttributes.sickChance_ = (this->sickChance_
-			+ secondAttributes.sickChance_) / 2.0
-			+ mutationDistribution(generator);
+newAttributes.sickChance_ = (this->sickChance_ + secondAttributes.sickChance_)
+		/ 2.0 + mutationDistribution(generator);
 
-	newAttributes.sightAngle_ = (this->sightAngle_
-			+ secondAttributes.sightAngle_) / 2.0
-			+ (mutationDistribution(generator) * 360.0 / 20.0);
+newAttributes.sightAngle_ = (this->sightAngle_ + secondAttributes.sightAngle_)
+		/ 2.0 + (mutationDistribution(generator) * 360.0 / 20.0);
 
-	newAttributes.sightLength_ = (this->sightLength_
-			+ secondAttributes.sightLength_) / 2.0
-			+ (mutationDistribution(generator) * 400.0 / 6.0);
+newAttributes.sightLength_ =
+		(this->sightLength_ + secondAttributes.sightLength_) / 2.0
+				+ (mutationDistribution(generator) * 400.0 / 6.0);
 
-	newAttributes.hearingRange_ = (this->hearingRange_
-			+ secondAttributes.hearingRange_) / 2.0
-			+ (mutationDistribution(generator) * 400.0 / 6.0);
+newAttributes.hearingRange_ = (this->hearingRange_
+		+ secondAttributes.hearingRange_) / 2.0
+		+ (mutationDistribution(generator) * 400.0 / 6.0);
 
-	newAttributes.eatNeed_ = (this->eatNeed_ + secondAttributes.eatNeed_) / 2.0
-			+ mutationDistribution(generator);
+newAttributes.eatNeed_ = (this->eatNeed_ + secondAttributes.eatNeed_) / 2.0
+		+ mutationDistribution(generator);
 
-	newAttributes.sleepNeed_ = (this->sleepNeed_ + secondAttributes.sleepNeed_)
-			/ 2.0 + mutationDistribution(generator);
+newAttributes.sleepNeed_ = (this->sleepNeed_ + secondAttributes.sleepNeed_)
+		/ 2.0 + mutationDistribution(generator);
 
-	newAttributes.lifeLen_ = (this->lifeLen_ + secondAttributes.lifeLen_) / 2.0
-			+ mutationLifeDistribution(generator);
+newAttributes.lifeLen_ = (this->lifeLen_ + secondAttributes.lifeLen_) / 2.0
+		+ mutationLifeDistribution(generator);
 
-	return newAttributes;
+return newAttributes;
 }
